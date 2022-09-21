3dc53c8c05116a7b4c508264819aa502d3c65c52
Govind Rajput <govindrajput9819@gmail.com>
2022-09-21 16:57:10 +0530

    first commit

--- src/app.ts
+++ src/app.ts
@@ -1,63 +1,135 @@
-// const userName = 'Govind'
-// userName = 'Rajput'
-// let age = 30;
+abstract class Department {
+    static fiscalYear = 2022;
+    // private readonly id: string;
+    // private name: string;
+    protected employees: string[] = [];
 
-// age = 29;
+    constructor(protected readonly id: string ,public name: string)  {
+        // this.id = id;
+        // this.name = n;
+        // console.log(Department.fiscalYear)
+    }
 
-function add(a: number, b: number) {
-    let result;
-    result = a + b;
-    return result;
+    static createEmployee(name: string) {
+        return { name: name };
+    }
+
+    abstract describe(this : Department) : void;
+    // {
+    //     // console.log(`Department(${this.id}): ${this.name}`)
+    // }
+
+    addEmployee(employee: string) {
+        //validation etc
+        // this.id = 'd2';  // not changes in id because it is readonly properties
+        this.employees.push(employee)
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees)
+    }
+
+}   
+
+class ITDepartment extends Department {
+    admins: string[];
+    constructor(id: string, admins : string[]) {
+      super(id, 'IT');
+      this.admins = admins;
+    }
+
+    describe(){
+        console.log('IT Department - ID: ' +this.id)
+    }
 }
 
-// if (age > 20) {
-//     let isOld= true;
-// }
+class AccountingDepartment extends Department {
+    private lastReport: string;
+    private static instance: AccountingDepartment;
 
-// console.log(isOld);
+    get mostRecentReport() {
+        if (this.lastReport) {
+            return this.lastReport
 
-const adds = (a: number, b: number) =>  a + b;
+        }
+        throw new Error ('No report found.')
+    }
 
-// const adds = (a: number, b: 2) =>  a + b;  //provide default parameter
+    set mostRecentReport(value: string) {
+        if(!value) {
+            throw new Error('Please pass in a valid value!')
+        }
+        this.addReport(value)
+    }
+    private constructor(id: string, private reports : string[]) {
+        super(id, 'Accounting');
+        this.lastReport = reports[0]
+      }
 
-const printOutput: (a: number | string) => void = output => console.log(output)
+      static getInstance() {
+        if (AccountingDepartment.instance) {
+            return this.instance;
+        }
+        this.instance = new AccountingDepartment('d2', [])
+        return this.instance;
+      }
 
+      describe() {
+          console.log("Accounting Department - ID: "+this.id)
+      }
 
+      addEmployee(name: string): void {
+           {
+            if(name === "Govind") {
+                return;
+            }
+            this.employees.push(name)
+          }
+      }
 
-const button = document.querySelector('button');
-
-if (button) {
-    button.addEventListener('click', event => console.log(event))
+      addReport(text: string) {
+        this.reports.push(text) 
+    this.lastReport = text;     }
+       
+        printReports() {
+            console.log(this.reports)
+        }
 }
 
-printOutput(add(5, 2))
+const employee1 = Department.createEmployee('Govind')
+console.log(employee1 , Department.fiscalYear)
 
-const hobbies = ['Sport', 'Cocking'];
-const activeHobbies = ['Hacking'];
- activeHobbies.push(...hobbies);
+const it = new ITDepartment('d1', ['govind'])
+it.addEmployee('Govind');
+it.addEmployee('Tanmay');
+// it.employees[2] = 'Abhi';  // not working with private keyword of employees
+it.describe()
+it.printEmployeeInformation()
 
- const person = {
-    firstName: 'Govind',
-    age : 24
- }
+console.log(it)
 
- const copiedPerson = { ...person };
+// const accounting = new AccountingDepartment('d2', [])
 
- const addNum = (...numbers: number[]) => {
-     return numbers.reduce((curResut, curValue) => {
-        return curResut + curValue;
-     }, 0);
+const accounting = AccountingDepartment.getInstance()
+const accounting2 = AccountingDepartment.getInstance()
+console.log(accounting, accounting2)
 
-     
- }
+accounting.mostRecentReport = 'Year end Report';
+accounting.addReport('Something went wrong...')
+console.log(accounting.mostRecentReport)
 
- const addedNumbers = addNum(5,10, 2, 3.7);
- console.log(addedNumbers)
+accounting.addEmployee("Govind")
 
- const [hobby1, hobby2, ...remainingHobbies] = hobbies;
 
- console.log(hobbies, hobby1, hobby2);
+accounting.addEmployee("Vikram")
 
- const { firstName: userName, age} = person;
+// accounting.printReports()
+// accounting.printEmployeeInformation()
+accounting.describe()
 
- console.log(userName, age)
+
+
+// const accountingCopy = { name: 'DUMMY', describe: it.describe };
+
+// accountingCopy.describe()
